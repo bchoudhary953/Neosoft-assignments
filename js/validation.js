016ecc53e8ca70f7c66e513acccc7716a41b60e9
Bhavana <bchoudhary953@gmail.com>
2020-10-06 16:06:48 +0530

    new  commit

--- js/validation.js
+++ js/validation.js
@@ -1,106 +1,109 @@
+$(document).ready(function(){
+	   $('form').submit(function(e) {
+	    e.preventDefault();
+	    var first_name = $('#firstname').val();
+	    var last_name = $('#lastname').val();
+	    var email = $('#email').val();
+	    var office = $('#office').val();
+	    var phone = $('#phone').val();
+	    var password = $('#password').val();
+	    var pwd = $('#pwd').val();
+	    var radioValue = $("input[name='gender']:checked").val();
+	    var checkValue = $("input[name='interest']:checked").val();
+	    $(".error").remove();
 
-      // Form validation code 
-      function validation() {
-      
-         if( document.myForm.firstname.value == "" ) {
-            alert( "Please provide your First name!" );
-            document.myForm.firstname.focus() ;
-            return false;
-         }
-	if( document.myForm.lastname.value == "" ) {
-            alert( "Please provide your last name!" );
-            document.myForm.lastname.focus() ;
-            return false;
-         }
-	if( document.myForm.phone.value == "" ) {
-            alert( "Please provide your phone number!" );
-            document.myForm.phone.focus() ;
-            return false;
-         }
-	
-	if( document.myForm.phone.value.length != 10 ) {
-            alert( "Please enter valid phone number!" );
-            document.myForm.phone.focus() ;
-            return false;
-         }
- 	
-
-	if( isNaN(document.myForm.phone.value)) {
-            alert( "Please enter valid phone number!" );
-            document.myForm.phone.focus() ;
-            return false;
-         }
-	
-	if( document.myForm.office.value == "" ) {
-            alert( "Please provide office number!" );
-            document.myForm.office.focus() ;
-            return false;
-         }
-	if( isNaN(document.myForm.office.value)) {
-            alert( "Please enter valid office number!" );
-            document.myForm.office.focus() ;
-            return false;
-	}
-	if( document.myForm.password.value == "" ) {
-            alert( "Please provide password!" );
-            document.myForm.password.focus() ;
-            return false;
-         }
-	
-	var pattern=  /^[A-Za-z]\w{8,12}$/;
-	if( !document.myForm.password.value.match(pattern) ) {
-            alert( "Password should contain only alphanumeric value and Range should be between 8 to 12 charachers!" );
-            document.myForm.password.focus() ;
-            return false;
-	}
-        if( document.myForm.pwd.value == "" ) {
-            alert( "Please confirm the password!" );
-            document.myForm.pwd.focus() ;
-            return false;
-         }   
-	
-	if( document.myForm.pwd.value != document.myForm.password.value ){
-	    alert( "Confirm password again!");
-	    document.myForm.pwd.focus() ;
-            return false;
-         }if(document.myForm.year.value == 0 || document.myForm.day.value == 0 || document.myForm.month.value == 0 ){
-	    alert("Enter DOB");
-	    return false;
-	}
-	if(! document.myForm.gender[0].checked && !document.myForm.gender[1].checked ){
-	    alert("enter gender");
-	    return false;
-	}  
-	
-	var group = document.myForm.interest;
-	for (var i=0; i<group.length; i++) {
-		if (group[i].checked)
-		break;
-	}
-	if (i==group.length)
-	return alert("Select one interest"); 
-	if( document.myForm.about.value == "" ) {
-            alert( "Please enter something about you!" );
-            document.myForm.about.focus() ;
-            return false;
-         } 
-	
-         if( document.myForm.email.value == "" ) {
-            alert( "Please provide your email!" );
-            document.myForm.email.focus() ;
-            return false;
-         } 
-	var atposition = document.myForm.email.value.indexOf("@");  
-	var dotposition = document.myForm.email.value.lastIndexOf(".");  
-	if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-	    alert("Please enter a valid email address");  
-	    document.myForm.email.focus();
- 	    return false;  
- 	 }  	
-}
-function getAge(){
+	    
+	    if (first_name.length < 1) {
+	     alert("Enter name");
+	     firstname.focus();
+	     return false;
+	    }
+	    if (last_name.length < 1) {
+	      alert("Enter last name");
+	      lastname.focus();
+	     return false;
+	    }
+	    
+	    if ($('#phone').val().length < 1) {
+	      alert("Enter phone number");
+	      phone.focus();
+	     return false;
+	    }
+	    else{
+		var validphone = /^[0-9]+$/.test(phone);
+		if (phone.length < 10 || !validphone ) {
+	      	   alert("Enter valid phone number");
+		   phone.focus();
+	           return false;
+		}
+	    }
+	   var valid = /^[0-9]+$/.test(office);
+		
+	   if( !valid){
+		alert("Enter valid office number");
+		office.focus();
+	        return false;
+	   }
+	   if(!radioValue){
+                alert("Enter gender");
+	        return false;
+            }
+	    if(!checkValue){
+                alert("Enter at least one interest");
+		return false;
+            }
+	    if ($('#password').val().length < 1) {
+	        alert("Enter password");
+		password.focus();
+	        return false;
+	    }
+	    else{
+		var pattern=  /^[A-Za-z]\w{8,12}$/;
+		if (!pattern.test(password)){
+		   alert("Password Range should be between 8 to 12 charachers, only Alphanumeric characters, No Special characters");
+		   password.focus();
+	           return false;
+		}
+	    }
+	    if (pwd.length < 1 || pwd != password ) {
+	        alert("Confirm the password");
+		pwd.focus();
+	        return false;
+	    }
+	    
+	    if ($('#month').val() == 0 || $('#day').val() == 0 || $('#year').val() == 0) {
+         	alert("Enter DOB");
+		return false;
+	    }
+	     
+	    if ($('#about').val().length < 1) {
+	        alert("Enter about you");
+		about.focus();
+	        return false;
+	    }
+	    if (email.length < 1) {
+	        alert("Enter email id");
+		email.focus();
+	        return false;
+	    } else {
+	    
+ 	      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	      var validEmail = regEx.test(email);
+	      if (!validEmail) {
+		alert("Enter valid email id");
+		email.focus();
+		return false;
+	      }
+	    }
+	   
+           return true;
+        
+        });
+});
+$(document).ready(function(){
+	$("#year").mouseleave(function(){
 	var today = new Date();
-	var birthday = new Date(document.myForm.year.value, document.myForm.month.value,document.myForm.day.value);
+	var birthday = new Date($("#year").val(), $("#month").val(),$("#day").val());
         
         var differenceInMilisecond = today - birthday;
        
@@ -113,11 +116,12 @@ function getAge(){
         day_age = day_age % 30;
         var age = year_age +"."+month_age;
 	
-         document.myForm.age.value = age;
-	
-} 
+        $("#age").val(age);
+	});
 
 
+   
+});
 
 
 
